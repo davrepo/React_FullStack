c29563fd9670aaaf2222e832692267021decc04c
volcanodoryshrimpnettuba <111703011+volcanodoryshrimpnettuba@users.noreply.github.com>
2023-10-04 14:35:48 +0200

    .

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -6,14 +6,17 @@ app.use(express.json()); // middleware to parse JSON
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [], 
   },
   {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
   },
   {
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
 }]
 
 // PUT /articles/learn-react/upvote
@@ -26,6 +29,20 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     res.send(`${name} now has ${article.upvotes} upvotes`);
   } else {
     res.status(404).send('Article not found');
+  }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articlesInfo.find(a => a.name === name);
+
+  if (article) {
+      article.comments.push({ postedBy, text });
+      res.send(article.comments);
+  } else {
+      res.send('That article doesn\'t exist!');
   }
 });
 
